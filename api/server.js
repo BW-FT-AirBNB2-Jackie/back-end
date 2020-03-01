7feef6c009d3b86967ed5ef26cdfb31b2416185b
Eric Ferguson <turtle0888@gmail.com>
2020-03-01 10:25:28 -0600

    Fixed some errors

--- api/server.js
+++ api/server.js
@@ -4,12 +4,16 @@ const helmet = require('helmet');
 
 const logger = require('../middleware/logger');
 
+const usersRouter = require('../users/users.router');
+
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use(logger);
+
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send('<h1>Its working</h1>');

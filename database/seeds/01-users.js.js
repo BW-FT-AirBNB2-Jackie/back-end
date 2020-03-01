f564f4bcf96e878249faa62bd51f397a0c4bbd84
Eric Ferguson <turtle0888@gmail.com>
2020-03-01 15:14:11 -0600

    Fixed user seed data

--- database/seeds/01-users.js.js
+++ database/seeds/01-users.js.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
     .del()
     .then(function() {
       return knex('users').insert([
-        { name: 'james hatfield', password: 'metallica' },
-        { name: 'angus young', password: 'acdc' },
-        { name: 'maynard keenan', password: 'tool' }
+        { username: 'james hatfield', password: 'metallica' },
+        { username: 'angus young', password: 'acdc' },
+        { username: 'maynard keenan', password: 'tool' }
       ]);
     });
 };

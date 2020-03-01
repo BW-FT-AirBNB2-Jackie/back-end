e32f3077fe4e7e94f39f9a77a5bd1b32e95601f6
Eric Ferguson <turtle0888@gmail.com>
2020-03-01 11:41:50 -0600

    Add login and register endpoints

--- api/server.js
+++ api/server.js
@@ -4,7 +4,9 @@ const helmet = require('helmet');
 
 const logger = require('../middleware/logger');
 
+const authRouter = require('../auth/auth-router');
 const usersRouter = require('../users/users.router');
+const restricted = require('../auth/authenticate-middleware');
 
 const server = express();
 
@@ -13,7 +15,8 @@ server.use(cors());
 server.use(express.json());
 server.use(logger);
 
-server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/users', restricted, usersRouter);
 
 server.get('/', (req, res) => {
   res.send('<h1>Its working</h1>');

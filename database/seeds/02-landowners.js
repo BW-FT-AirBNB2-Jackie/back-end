a7a5a286ce92ab3d56fe0f5c26c84e6c4d8d387a
Eric Ferguson <turtle0888@gmail.com>
2020-03-03 12:41:01 -0600

    fixed seed errors

--- database/seeds/02-landowners.js
+++ database/seeds/02-landowners.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('landowners')
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex('landowners').insert([

77dffea1f9ebf176682a05aed11ce7b5907dfb72
Eric Ferguson <turtle0888@gmail.com>
2020-03-02 18:04:00 -0600

    Add bookings migrations

---
+++ database/migrations/20200302175651_bookings.js
@@ -0,0 +1,27 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('bookings', bookings => {
+    bookings.primary(['users_id', 'listing_id']);
+
+    bookings
+      .integer('users_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+
+    bookings
+      .integer('listings_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('listings')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('bookings');
+};
